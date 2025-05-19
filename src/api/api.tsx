a994317adb9238fed5be0ca3e0f6c8ee0e3ea7cc
kiran <kirankumar2003g@gmail.com>
2025-05-19 10:43:25 +0530

    Fetching API data with React Query

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -4,7 +4,14 @@ const api = axios.create({
     baseURL: "https://jsonplaceholder.typicode.com",
 })
 
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
 // To fetch the data:
-export const fetchPosts = () => {
-    return api.get("/posts");
+export const fetchPosts = async () : Promise<Post[]> => {
+    const resp =  await api.get("/posts");
+    return resp.status === 200 ? resp.data : [];
 }
--- src/pages/FetchRQ.tsx
+++ src/pages/FetchRQ.tsx
@@ -1,5 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchPosts } from "../api/api";
+
 const FetchRQ = () => {
-  return <div>Hello Fetch RQ</div>;
+  const { data } = useQuery({
+    queryKey: ["posts"], // Use State
+    queryFn: fetchPosts, // Use Effect
+  });
+
+  return (
+    <div>
+      <ul className="section-accordion">
+        {data?.map((post) => {
+          // Array Destructuring:
+          const { id, title, body } = post;
+          return (
+            <li key={id}>
+              <h3>{title}</h3>
+              <p>{body}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default FetchRQ;
